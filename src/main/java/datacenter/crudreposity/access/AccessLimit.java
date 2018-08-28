b8887bd09aa2bcae8c18148558a14894639a83ee
jason <shicong.zhang@juniorchina.com>
2018-08-28 10:34:17 +0800

    添加过滤器Intereptor实现限流次数拦截和接口参数初始化，主要应用用户信息处理

--- src/main/java/datacenter/crudreposity/access/AccessLimit.java
+++ src/main/java/datacenter/crudreposity/access/AccessLimit.java
@@ -9,5 +9,7 @@ import java.lang.annotation.Target;
 @Retention(RUNTIME)
 @Target(METHOD)
 public @interface AccessLimit {
+    //接口注解绑定时候的参数  例如：@AccessLimit(seconds = 30) 这里模拟的就是接口限流操作，
+    //return true:说明验证通过，可以继续访问此接口，return false:说明验证不通过，不允许访问此接口。
     int seconds();
  }
