2285c917292501f8ae27c29c319e145fd564a79b
jason <shicong.zhang@juniorchina.com>
2018-09-19 14:19:47 +0800

    图片裁剪

--- src/main/java/datacenter/crudreposity/util/UserUtil.java
+++ src/main/java/datacenter/crudreposity/util/UserUtil.java
@@ -2,8 +2,13 @@ package datacenter.crudreposity.util;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
+import net.coobird.thumbnailator.Thumbnails;
+import org.apache.commons.codec.binary.Base64;
+import sun.security.provider.MD5;
 
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
 import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -15,8 +20,26 @@ import java.util.Date;
 import java.util.List;
 
 public class UserUtil {
-	
 
+	//裁剪图片处理
+	private String getThumnails(String code){
+		try {
+			byte [] imageData = null; //获取二进制源文件流binFileService.getBinFile(rowkey).getData();
+			if(imageData == null){
+				return "";
+			}
+			ByteArrayInputStream intputStream = new ByteArrayInputStream(imageData);
+			Thumbnails.Builder<? extends InputStream> builder = Thumbnails.of(intputStream).size(100, 100);
+			BufferedImage bufferedImage = builder.asBufferedImage();
+			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			ImageIO.write(bufferedImage, "png", baos);
+			byte[] byteArray = baos.toByteArray();
+			return Base64.encodeBase64String(byteArray);
+		}catch (Exception ex){
+			ex.printStackTrace();
+		}
+		return "";
+	}
 	
 	public static void main(String[] args)throws Exception {
 
