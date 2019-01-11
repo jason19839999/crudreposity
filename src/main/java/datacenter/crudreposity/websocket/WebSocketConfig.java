bf0364a594592965f491c3c42a76365e61a8f6cc
jason <shicong.zhang@juniorchina.com>
2019-01-11 19:20:58 +0800

    test

--- src/main/java/datacenter/crudreposity/websocket/WebSocketConfig.java
+++ src/main/java/datacenter/crudreposity/websocket/WebSocketConfig.java
@@ -9,10 +9,10 @@ import org.springframework.web.socket.server.standard.ServerEndpointExporter;
  * 创建者  科帮网
  * 创建时间	2018年5月29日
  */
-@Configuration
+//@Configuration
 public class WebSocketConfig {  
-    @Bean
-    public ServerEndpointExporter serverEndpointExporter() {  
-        return new ServerEndpointExporter();  
-    }  
+//    @Bean
+//    public ServerEndpointExporter serverEndpointExporter() {
+//        return new ServerEndpointExporter();
+//    }
 }  
