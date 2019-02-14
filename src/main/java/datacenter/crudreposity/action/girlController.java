59211e53f6958c705ddd8101b5c71b4174a50067
jason <zhangshicong888@sina.com>
2019-02-14 17:08:44 +0800

    test

--- src/main/java/datacenter/crudreposity/action/girlController.java
+++ src/main/java/datacenter/crudreposity/action/girlController.java
@@ -141,7 +141,7 @@ public class girlController {
     @RequestMapping(value = "getRedis", method = RequestMethod.GET)
     @ResponseBody
     public String getRedis() {
-
+        logger.info("进入redis");
         //objgirlInfoRedisDao.save("set_age");
         String result = objgirlInfoRedisDao.read("name");
         //zadd set_age 100 10 100 9 100 8   100为SCORES，必须为整数 10/9/8 可以为数组或者字符串
